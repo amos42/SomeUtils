56721ebccbaec32222c6a2a2f048b2d9bf0ab802
amos42 <46707197+amos42@users.noreply.github.com>
2019-12-16 13:56:50 +0900

    Update MacroUtil.cs

--- MacroUtil.cs
+++ MacroUtil.cs
@@ -6,10 +6,11 @@ Last Update : 2019-04-24 오후 1:08:08
 */
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.IO;
 using System.Text;
 
-namespace Synergy.Base
+namespace DevPlatform.Base
 {
     /// <summary>
     /// 문자열이나 숫자 배열을 다양한 형태의 데이터로 변환하는 유틸리티
@@ -21,16 +22,33 @@ namespace Synergy.Base
         public const string DEFAULT_NAMESPACE_SEP = ".";
         public const int BUFFER_SIZE = 1024 * 8;
 
-        private static readonly ILogger logger = Logger.GetLogger();
+        //private static readonly ILogger logger = LoggerFactory.GetLogger();
 
+        /// <summary>
+        /// IRunner 인터페이스
+        /// </summary>
         public interface IRunner
         {
             string Run(string macroKey);
         }
 
-        public static int RunMacro(Stream inputStream, Stream outputStream, string startLiter, string endLiter, 
+        /// <summary>
+        /// Stream 입력을 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="inputStream">입력 스트림</param>
+        /// <param name="outputStream">출력 스트림</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <param name="nameSpace">매크로명의 namespace명</param>
+        /// <param name="nameSpaceSep">namespace의 구분자</param>
+        /// <param name="runner">매크로 치환 수행 함수</param>
+        /// <param name="invalideStr">무효한 매크로일 경우 치환할 문자열</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
+        public static int RunMacro(Stream inputStream, Stream outputStream, string startLiter, string endLiter,
             string nameSpace, string nameSpaceSep, IRunner runner, string invalideStr)
         {
+            if (inputStream == null || outputStream == null) return 0;
+
             try
             {
                 /*
@@ -54,22 +72,34 @@ namespace Synergy.Base
             }
             catch (IOException ex)
             {
-                logger.Error(ex.Message);
+                ILogger logger = LoggerFactory.GetLogger();
+                logger?.Error(ex.Message, true);
                 return 0;
             }
 
             return 1;
         }
 
-        public static string RunMacro(string source, string startLiter, string endLiter, 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <param name="nameSpace">매크로명의 namespace명</param>
+        /// <param name="nameSpaceSep">namespace의 구분자</param>
+        /// <param name="runner">매크로 치환 수행 함수</param>
+        /// <param name="invalideStr">무효한 매크로일 경우 치환할 문자열</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
+        public static string RunMacro(string source, string startLiter, string endLiter,
             string nameSpace, string nameSpaceSep, IRunner runner, string invalideStr)
         {
-            if (String.IsNullOrEmpty(source))
+            if (String.IsNullOrEmpty(source) || startLiter == null || endLiter == null)
             {
                 return source;
             }
 
-            if (source.IndexOf(startLiter) < 0)
+            if (source.IndexOf(startLiter, StringComparison.Ordinal) < 0)
             {
                 return source;
             }
@@ -91,13 +121,13 @@ namespace Synergy.Base
             {
                 nameSpace = null;
             }
-            string str = "";
+            var sb = new StringBuilder();
 
             int len = source.Length;
             int idx = 0;
             while (idx < len)
             {
-                int idx2 = source.IndexOf(startLiter, idx);
+                int idx2 = source.IndexOf(startLiter, idx, StringComparison.Ordinal);
                 if (idx2 < 0)
                 {
                     break;
@@ -105,10 +135,10 @@ namespace Synergy.Base
 
                 if (idx < idx2)
                 {
-                    str += source.Substring(idx, idx2 - idx);
+                    sb.Append(source.Substring(idx, idx2 - idx));
                 }
 
-                int idx3 = source.IndexOf(endLiter, idx2);
+                int idx3 = source.IndexOf(endLiter, idx2, StringComparison.Ordinal);
                 if (idx3 >= 0)
                 {
                     if (runner != null)
@@ -116,9 +146,9 @@ namespace Synergy.Base
                         string label = source.Substring(idx2 + startLiterLen, idx3 - idx2 - startLiterLen);
                         if (!String.IsNullOrEmpty(nameSpace))
                         {
-                            if (!label.StartsWith(nameSpace))
+                            if (!label.StartsWith(nameSpace, StringComparison.Ordinal))
                             {
-                                str += startLiter + label + endLiter;
+                                sb.Append(startLiter + label + endLiter);
                                 label = null;
                             }
                         }
@@ -128,13 +158,13 @@ namespace Synergy.Base
                             if (value != null)
                             {
                                 value = RunMacro(value, startLiter, endLiter, nameSpace, nameSpaceSep, runner, invalideStr);
-                                str += value;
+                                sb.Append(value);
                             }
                             else
                             {
                                 if (invalideStr != null)
                                 {
-                                    str += invalideStr;
+                                    sb.Append(invalideStr);
                                 }
                             }
                         }
@@ -149,17 +179,28 @@ namespace Synergy.Base
             }
             if (idx < len)
             {
-                str += source.Substring(idx);
+                sb.Append(source.Substring(idx));
             }
 
-            return str;
+            return sb.ToString();
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <param name="runner">매크로 치환 수행 함수</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string RunMacro(string source, string startLiter, string endLiter, IRunner runner)
         {
             return RunMacro(source, startLiter, endLiter, null, null, runner, null);
         }
 
+        /// <summary>
+        /// 매크로 적용하는 IRunner 구현체
+        /// </summary>
         private class MacroRunner : IRunner
         {
             private IDictionary<string, object> macros;
@@ -171,56 +212,198 @@ namespace Synergy.Base
 
             public string Run(string macroKey)
             {
-                if(macros.TryGetValue(macroKey, out var value))
+                if (macros.TryGetValue(macroKey, out var value))
                 {
                     return value as string;
-                } else
+                }
+                else
                 {
                     return null;
                 }
             }
         }
 
+        /// <summary>
+        /// 매크로 컬렉션이 NameValueCollection일 경우의 IRunner 구현체
+        /// </summary>
+        private class MacroRunner2 : IRunner
+        {
+            private NameValueCollection macros;
+
+            public MacroRunner2(NameValueCollection macros)
+            {
+                this.macros = macros;
+            }
+
+            public string Run(string macroKey)
+            {
+                var value = macros.Get(macroKey);
+                if (value != null && value is string)
+                {
+                    return value as string;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <param name="nameSpace">매크로명의 namespace명</param>
+        /// <param name="nameSpaceSep">namespace의 구분자</param>
+        /// <param name="runner">매크로 치환 수행 함수</param>
+        /// <param name="invalideStr">무효한 매크로일 경우 치환할 문자열</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string ProcessMacro(string source, string startLiter, string endLiter,
                 string nameSpace, string nameSpaceSep, IRunner runner, string invalideStr)
         {
             return RunMacro(source, startLiter, endLiter, nameSpace, nameSpaceSep, runner, invalideStr);
         }
 
+        /// <summary>
+        /// Stream 입력을 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="outputStream"></param>
+        /// <param name="startLiter"></param>
+        /// <param name="endLiter"></param>
+        /// <param name="nameSpace"></param>
+        /// <param name="nameSpaceSep"></param>
+        /// <param name="runner"></param>
+        /// <param name="invalideStr"></param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static int ProcessMacro(Stream inputStream, Stream outputStream, string startLiter, string endLiter,
                 string nameSpace, string nameSpaceSep, IRunner runner, string invalideStr)
         {
             return RunMacro(inputStream, outputStream, startLiter, endLiter, nameSpace, nameSpaceSep, runner, invalideStr);
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <param name="runner">매크로 치환 수행 함수</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string ProcessMacro(string source, string startLiter, string endLiter, IRunner runner)
         {
             return RunMacro(source, startLiter, endLiter, null, null, runner, null);
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="macros">매크로 사전</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string ProcessMacro(string source, IDictionary<string, object> macros, string startLiter, string endLiter)
         {
             return RunMacro(source, startLiter, endLiter, new MacroRunner(macros));
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="macros">매크로 사전</param>
+        /// <param name="startLiter">매크로 시작 식별 문자</param>
+        /// <param name="endLiter">매크로 종료 식별 문자</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
+        public static string ProcessMacro(string source, NameValueCollection macros, string startLiter, string endLiter)
+        {
+            return RunMacro(source, startLiter, endLiter, new MacroRunner2(macros));
+        }
+
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="runner">매크로 치환 수행 함수</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string ProcessMacro(string source, IRunner runner)
         {
             return ProcessMacro(source, DEFAULT_START_LITER, DEFAULT_END_LITER, runner);
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="macros">매크로 사전</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string ProcessMacro(string source, IDictionary<string, object> macros)
         {
             return ProcessMacro(source, macros, DEFAULT_START_LITER, DEFAULT_END_LITER);
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="macros">매크로 사전</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
+        public static string ProcessMacro(string source, NameValueCollection macros)
+        {
+            return ProcessMacro(source, macros, DEFAULT_START_LITER, DEFAULT_END_LITER);
+        }
+
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="nameSpace"></param>
+        /// <param name="macros">매크로 사전</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static string ProcessMacro(string source, string nameSpace, IDictionary<string, object> macros)
         {
             return ProcessMacro(source, DEFAULT_START_LITER, DEFAULT_END_LITER, nameSpace, null, new MacroRunner(macros), null);
         }
 
+        /// <summary>
+        /// 문자열을 입력 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="source">입력 문자열</param>
+        /// <param name="nameSpace"></param>
+        /// <param name="macros">매크로 사전</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
+        public static string ProcessMacro(string source, string nameSpace, NameValueCollection macros)
+        {
+            return ProcessMacro(source, DEFAULT_START_LITER, DEFAULT_END_LITER, nameSpace, null, new MacroRunner2(macros), null);
+        }
+
+        /// <summary>
+        /// Stream 입력을 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="outputStream"></param>
+        /// <param name="nameSpace"></param>
+        /// <param name="macros">매크로 사전</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
         public static int ProcessMacro(Stream inputStream, Stream outputStream, string nameSpace, IDictionary<string, object> macros)
         {
             return ProcessMacro(inputStream, outputStream, DEFAULT_START_LITER, DEFAULT_END_LITER, nameSpace, null, new MacroRunner(macros), null);
+        }
+
+        /// <summary>
+        /// Stream 입력을 받아 매크로 프로세스를 진행합니다.
+        /// </summary>
+        /// <param name="inputStream"></param>
+        /// <param name="outputStream"></param>
+        /// <param name="nameSpace"></param>
+        /// <param name="macros">매크로 사전</param>
+        /// <returns>매크로 처리 결과 문자열</returns>
+        public static int ProcessMacro(Stream inputStream, Stream outputStream, string nameSpace, NameValueCollection macros)
+        {
+            return ProcessMacro(inputStream, outputStream, DEFAULT_START_LITER, DEFAULT_END_LITER, nameSpace, null, new MacroRunner2(macros), null);
         }
     }
 

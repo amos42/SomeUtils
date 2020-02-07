e715c5d252bb224a79e4ef256624131a159b9d00
amos42 <46707197+amos42@users.noreply.github.com>
2020-02-07 16:24:51 +0900

    Update ApplyUpdateVersion.py

--- scripts/vs-proj/ApplyUpdateVersion.py
+++ scripts/vs-proj/ApplyUpdateVersion.py
@@ -10,13 +10,19 @@ def getArguments():
     parser = argparse.ArgumentParser()
     parser.add_argument(dest='solutionFilename', help='Example) Sample.sln')
     parser.add_argument('--change-packages', '-c', nargs='+', default=[], metavar='"PackageName Version"', dest='changePackageList', help='Example) "DevPlatfomr.Base 1.0.1" "DevPlatfomr.DB 1.0.6"')
-    parser.add_argument('--assembly-change-packages', '-a', nargs='*', default=[], metavar='"PackageName"', dest='assemblyChangePackageList', help='Example) DevPlatfomr.Base DevPlatfomr.DB DevPlatfomr.DB.*')
+    parser.add_argument('--prerelease-signature', '-p', default="", metavar='alpha|beta|rc', dest='prereleaseSignature', help='Example) alpha1')
+    parser.add_argument('--assembly-change-packages', '-a', nargs='*', default=[], metavar='PackageName', dest='assemblyChangePackageList', help='Example) DevPlatfomr.Base DevPlatfomr.DB.*')
+    parser.add_argument('--assemblyfile-change-packages', '-f', nargs='*', default=[], metavar='PackageName', dest='assemblyFileChangePackageList', help='Example) DevPlatfomr.Base DevPlatfomr.DB.*')
+    parser.add_argument('--exclude-packages', '-x', nargs='*', default=[], metavar='PackageName', dest='excludePackageList', help='Example) DevPlatfomr.Base DevPlatfomr.DB.*')
 
     solutionFilename = parser.parse_args().solutionFilename
     changePackageList = parser.parse_args().changePackageList
+    prereleaseSignature = parser.parse_args().prereleaseSignature
     assemblyChangePackageList = parser.parse_args().assemblyChangePackageList
+    assemblyFileChangePackageList = parser.parse_args().assemblyFileChangePackageList
+    excludePackageList = parser.parse_args().excludePackageList
 
-    return solutionFilename, changePackageList, assemblyChangePackageList
+    return solutionFilename, changePackageList, prereleaseSignature, assemblyChangePackageList, assemblyFileChangePackageList, excludePackageList
 
 
 def setNewVersion(refInfo, newVersion):
@@ -38,11 +44,11 @@ def setProjectNewVersion(project, newVersion, changeList):
     return result
 
 
-def incProjectVersion(project, changeList):
+def incProjectVersion(project, changeList, presig = "" ):
     if (project.projRefInfo.newVersion == None) or (not project.projRefInfo.newVersion.core):
         if project.projRefInfo.version != None:
             newVersion = project.projRefInfo.version.clone()
-            newVersion.incTailVersion()
+            newVersion.incTailVersion(presig)
         else:
             newVersion = vsver.SemVersion("1.0.1")
         return setProjectNewVersion(project, newVersion, changeList)
@@ -56,13 +62,13 @@ def setProjectNewVersionByName(projDict, moduleName, newVersion, changeList):
     return setProjectNewVersion(proj, newVersion, changeList)
 
 
-def applyChangeProjectList(changeList, projDict, assemblyChangePackageList):
+def applyChangeProjectList(changeList, projDict, assemblyChangePackageList, assemblyFileChangePackageList, excludePackageList):
     for proj in changeList:
         if proj.projRefInfo.projectPath == None: continue
-        vsproj.updateProjectInfo(proj, projDict, assemblyChangePackageList)
+        vsproj.updateProjectInfo(proj, projDict, assemblyChangePackageList, assemblyFileChangePackageList, excludePackageList)
 
 
-def analizeProjectList(projList, projDict, changeList):
+def analizeProjectList(projList, projDict, changeList, presig = ""):
     if not changeList: return
 
     while True:
@@ -84,13 +90,13 @@ def analizeProjectList(projList, projDict, changeList):
                         changeCnt = changeCnt + 1
                         break
             if changeCnt > 0:
-                if incProjectVersion(proj, changeList): changeProjCnt = changeProjCnt + 1
+                if incProjectVersion(proj, changeList, presig): changeProjCnt = changeProjCnt + 1
 
         if changeProjCnt <= 0: break
 
 
 if __name__ == '__main__':
-    solutionFilename, changePackageList, assemblyChangePackageList = getArguments()
+    solutionFilename, changePackageList, prereleaseSignature, assemblyChangePackageList, assemblyFileChangePackageList, excludePackageList = getArguments()
 
     solInfo = vssol.getSolutionInfo(solutionFilename)
 
@@ -108,7 +114,7 @@ if __name__ == '__main__':
             proj.projRefInfo.newVersion = newVersion
             changelist.append(proj)
     
-    analizeProjectList(solInfo.projectList, solInfo.projectDict, changelist)
+    analizeProjectList(solInfo.projectList, solInfo.projectDict, changelist, prereleaseSignature)
         
     print("---------------------------")
     for projInfo in changelist:
@@ -128,6 +134,6 @@ if __name__ == '__main__':
         print("  * test project :", projInfo.isTestProject)
         print("---------------------------")
 
-    applyChangeProjectList(changelist, solInfo.projectDict, assemblyChangePackageList)
+    applyChangeProjectList(changelist, solInfo.projectDict, assemblyChangePackageList, assemblyFileChangePackageList, excludePackageList)
 
     print("All Done.")
